76416c7cf3a34175a62c11488c1f3a3f52acf49a
SirProg <kevinmaldonadoparedes@gmail.com>
2025-10-19 23:09:17 -0500

    Add

---
+++ game/scenes/ui/menu_pausa.gd
@@ -0,0 +1,10 @@
+extends AcceptDialog
+
+#func _ready():
+	# Conecta el botón al cambio de escena
+#	var btn = $BtnIrAOpciones
+#	btn.pressed.connect(_on_btn_ir_a_escena_pressed)
+
+#func _on_btn_ir_a_escena_pressed():
+#	get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
+#	hide()  # Cierra el popup antes de cambiar (buena práctica)
--- game/scripts/systems/ui/mobile_ui.gd
+++ game/scripts/systems/ui/mobile_ui.gd
@@ -10,12 +10,19 @@ extends CanvasLayer
 @onready var health_progress = $MarginContainer/HBoxContainer/VBoxContainer/TextureProgressBar
 @onready var stamina_progress = $MarginContainer/HBoxContainer/VBoxContainer/TextureProgressBar2
 
+#@onready var menu_pausa = preload("res://scenes/ui/MenuPausa.tscn")
+
 func _ready():
-    btn_left.pressed.connect(InputManager.on_move_left_press)
-    btn_left.released.connect(InputManager.on_move_left_release)
-    btn_right.pressed.connect(InputManager.on_move_right_press)
-    btn_right.released.connect(InputManager.on_move_right_release)
-    btn_jump.pressed.connect(InputManager.on_jump_press)
-    btn_shoot.pressed.connect(InputManager.on_shoot_press)
-    btn_special.pressed.connect(InputManager.on_special_press)
-    btn_melee.pressed.connect(InputManager.on_melee_press)
+	btn_left.pressed.connect(InputManager.on_move_left_press)
+	btn_left.released.connect(InputManager.on_move_left_release)
+	btn_right.pressed.connect(InputManager.on_move_right_press)
+	btn_right.released.connect(InputManager.on_move_right_release)
+	btn_jump.pressed.connect(InputManager.on_jump_press)
+	btn_shoot.pressed.connect(InputManager.on_shoot_press)
+	btn_special.pressed.connect(InputManager.on_special_press)
+	btn_melee.pressed.connect(InputManager.on_melee_press)
+
+#func _on_menu_pressed() -> void:
+#	var popup = menu_pausa.instantiate()
+#	add_child(popup)
+#	popup.popup_centered()  # Abre centrado
